272596b4247f7457ba072f23375371372118d3ce
zqh-hub <going163@163.com>
2021-06-22 21:29:29 +0800

    多线程

---
+++ process_001/process_003_多线程卖票.py
@@ -0,0 +1,25 @@
+import threading
+import time
+
+ticket = 100
+lock = threading.Lock()
+
+
+def sell_ticket():
+    global ticket
+    while True:
+        lock.acquire()  # 加同步锁
+        if ticket > 0:
+            ticket -= 1
+            lock.release()  # 每执行完一个卖票的动作就放开
+            print("{},此时ticket是：{}".format(threading.current_thread().name, ticket))
+        else:
+            lock.release()  # 所有的票卖完了，锁释放开
+            print("没票了")
+            break
+
+
+t1 = threading.Thread(target=sell_ticket, name="线程1")
+t2 = threading.Thread(target=sell_ticket, name="线程2")
+t1.start()
+t2.start()
